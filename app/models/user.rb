7bb7118261ec6277242753a0c9e2caee3f0f002a
ruu2023 <125800583+ruu2023@users.noreply.github.com>
2023-09-19 19:19:31 +0900

    モデル単体テストの追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates :password,format: { with: VALID_PASSWORD_REGEX, message: 'は半角英数を両方含む必要があります'}, length: { minimum: 6 }
+  validates :password,format: { with: VALID_PASSWORD_REGEX, message: 'は半角英数を両方含む必要があります', allow_blank: true}
   validates :nickname, presence: true
-  validates :surname, presence: true, format:{with:/\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/,message:"は全角ひらがな、全角カタカナ、漢字でご入力下さい。"}
-  validates :name, presence: true, format:{with:/\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/,message:"は全角ひらがな、全角カタカナ、漢字でご入力下さい。"}
-  validates :surname_kana, presence: true, format:{with:/\A[ァ-ヶー－]+\z/,message:"は全角カタカナでご入力下さい。"}
-  validates :name_kana, presence: true, format:{with:/\A[ァ-ヶー－]+\z/,message:"は全角カタカナでご入力下さい。"}
+  validates :surname, presence: true, format:{with:/\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/,message:"は全角ひらがな、全角カタカナ、漢字でご入力下さい。", allow_blank: true}
+  validates :name, presence: true, format:{with:/\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/,message:"は全角ひらがな、全角カタカナ、漢字でご入力下さい。", allow_blank: true}
+  validates :surname_kana, presence: true, format:{with:/\A[ァ-ヶー－]+\z/,message:"は全角カタカナでご入力下さい。", allow_blank: true}
+  validates :name_kana, presence: true, format:{with:/\A[ァ-ヶー－]+\z/,message:"は全角カタカナでご入力下さい。", allow_blank: true}
   validates :birthday, presence: true
 end
 
