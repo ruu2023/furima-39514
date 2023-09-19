2982d86b68ef6569ddf6ae1c39acf354ae673b8a
ruu2023 <125800583+ruu2023@users.noreply.github.com>
2023-09-19 17:55:10 +0900

    単体テストの記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates :password, format: { with: VALID_PASSWORD_REGEX, message: 'は半角英数を両方含む必要があります'},length: { minimum: 6 }
+  validates :password,format: { with: VALID_PASSWORD_REGEX, message: 'は半角英数を両方含む必要があります'}, length: { minimum: 6 }
   validates :nickname, presence: true
-  validates :surname, presence: true, format:{with:/\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/,message:"は全角ひらがな、全角カタカナ、漢字（でご入力下さい。"}
-  validates :name, presence: true, format:{with:/\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/,message:"は全角ひらがな、全角カタカナ、漢字（でご入力下さい。"}
-  validates :surname_kana, presence: true
-  validates :name_kana, presence: true
+  validates :surname, presence: true, format:{with:/\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/,message:"は全角ひらがな、全角カタカナ、漢字でご入力下さい。"}
+  validates :name, presence: true, format:{with:/\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/,message:"は全角ひらがな、全角カタカナ、漢字でご入力下さい。"}
+  validates :surname_kana, presence: true, format:{with:/\A[ァ-ヶー－]+\z/,message:"は全角カタカナでご入力下さい。"}
+  validates :name_kana, presence: true, format:{with:/\A[ァ-ヶー－]+\z/,message:"は全角カタカナでご入力下さい。"}
   validates :birthday, presence: true
 end
+
---
+++ spec/factories/users.rb
@@ -0,0 +1,16 @@
+FactoryBot.define do
+  factory :user do
+  transient do
+    person { Gimei.name }
+  end
+    nickname              {Faker::Name.initials(number: 2)}
+    email                 {Faker::Internet.email}
+    password              {Faker::Internet.password(min_length: 6)}
+    password_confirmation {password}
+    surname { person.first.kanji }
+    name { person.last.kanji }
+    surname_kana { person.first.katakana }
+    name_kana { person.last.katakana }
+    birthday { Faker::Date.backward }
+  end
+end
