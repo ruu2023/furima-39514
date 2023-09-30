4eb643029dd90b6b0a80a09a0bdb741e9c60807b
ruu2023 <125800583+ruu2023@users.noreply.github.com>
2023-09-30 15:16:13 +0900

    ActiveHashの登録つづき

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,15 +2,19 @@ class Item < ApplicationRecord
   validates :title, presence: true
   validates :description, presence: true
   validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :condition_id, presence: true
-  validates :charge_id, presence: true
-  validates :prefecture_id, presence: true
-  validates :day_id, presence: true
+  validates :condition_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :charge_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :day_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :price, presence: true
   belongs_to :user
   # has_one :record
 
   has_one_attached :image
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :genre
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :charge
+  belongs_to :prefecture
+  belongs_to :day
 end
