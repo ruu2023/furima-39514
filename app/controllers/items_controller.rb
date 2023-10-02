37621fe47d232930b727ce320dbf4cbb7d742ff2
ruu2023 <125800583+ruu2023@users.noreply.github.com>
2023-10-02 17:01:26 +0900

    任意修正箇所の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,12 +1,10 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!
 
   def index
   end
 
   def new
-    unless user_signed_in?
-      redirect_to new_user_session_path
-    end
     @item = Item.new
   end
 
