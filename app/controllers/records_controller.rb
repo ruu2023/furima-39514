48ce1dfd305a879219b89443877ba87d7c41b80b
ruu2023 <125800583+ruu2023@users.noreply.github.com>
2023-11-01 21:20:41 +0900

    フォーム・コントローラーの編集

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,3 +1,32 @@
 class RecordsController < ApplicationController
   before_action :authenticate_user!, except: :index
+
+  def index
+    @record_address = RecordAddress.new
+    set_item
+  end
+
+  def new
+  end
+
+  def create
+    @record_address = RecordAddress.new(record_params)
+    if @record_address.valid?
+      @record_address.save
+      redirect_to root_path
+    else
+      set_item
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def record_params
+    params.require(:record_address).permit(:post_code, :prefecture_id, :municipalities, :block, :apartment, :telephone).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+
 end
--- app/models/record_address.rb
+++ app/models/record_address.rb
@@ -1,4 +1,22 @@
 class RecordAddress
   include ActiveModel::Model
-  attr_accessor :user, :item, :post_code, :prefecture_id, :municipalities, :block, :apartment, :telephone, :record
+  attr_accessor :post_code, :prefecture_id, :municipalities, :block, :apartment, :telephone, :record_id, :item_id, :user_id
+
+  with_options presence: true do
+    validates :block
+    validates :municipalities
+    validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :telephone, format: {with: /\A[0-9]{10,11}\z/, message: "is invalid."}
+  end
+  validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
+
+
+  def save
+    # 寄付情報を保存し、変数donationに代入する
+    record = Record.create(user_id: user_id, item_id: item_id)
+    binding.pry
+    # 住所を保存する
+    # donation_idには、変数donationのidと指定する
+    Address.create(post_code: post_code, prefecture_id: prefecture_id, municipalities: municipalities, block: block, apartment: apartment, telephone: telephone, record_id: record.id)
+  end
 end
